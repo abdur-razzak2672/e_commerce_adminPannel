ea25a095bbeb70d74858985b25767c23db54636a
Abdur Razzak <73746963+abdur-razzak2672@users.noreply.github.com>
2022-03-21 23:09:51 +0600

    nothing commit

---
+++ ecommerce_adminPannel/product/views/product.py
@@ -0,0 +1,24 @@
+from django.views import generic
+
+from product.models import Variant
+
+
+class CreateProductView(generic.TemplateView):
+    template_name = 'products/create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context['product'] = True
+        context['variants'] = list(variants.all())
+        return context
+
+class listProductView(generic.TemplateView):
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(listProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context['product'] = True
+        context['variants'] = list(variants.all())
+        return context
